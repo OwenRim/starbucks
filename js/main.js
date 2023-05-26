c69909681fe32b2e84b498160d3fbff97db69b4b
owen <owen.rim@edwardsvacuum.com>
2023-05-26 17:07:29 +0900

    30. 스크롤위치계산 애니메이션(2)

--- js/main.js
+++ js/main.js
@@ -105,53 +105,77 @@ new Swiper('.promotion .swiper-container', {
     prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
     nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
-});
-
-const promotionEl = document.querySelector('.promotion');
-const promotionToggleBtn = document.querySelector('.toggle-promotion');
-let isHidePromotion = false;
-promotionToggleBtn.addEventListener('click', function () {
-  //promotion을 보여주거나 숨기는기능
-  isHidePromotion = !isHidePromotion
-  if (isHidePromotion) {
-    // 숨김처리!
-    promotionEl.classList.add('hide');
-  } else {
-    // 보임 처리!
-    promotionEl.classList.remove('hide');
+})
+new Swiper('.awards .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.awards .swiper-next' // 다음 버튼 선택자
   }
-});
+})
 
+
+/**
+ * Promotion 슬라이드 토글 기능
+ */
+// 슬라이드 영역 요소 검색!
+const promotionEl = document.querySelector('.promotion')
+// 슬라이드 영역를 토글하는 버튼 검색!
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+// 슬라이드 영역 숨김 여부 기본값!
+let isHidePromotion = false
+// 토글 버튼을 클릭하면,
+promotionToggleBtn.addEventListener('click', function () {
+  // 슬라이드 영역 숨김 여부를 반댓값으로 할당!
+  isHidePromotion = !isHidePromotion
+  // 요소를 숨겨야 하면,
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  // 요소가 보여야 하면,
+  } else {
+    promotionEl.classList.remove('hide')
+  }
+})
+
+
+/**
+ * 부유하는 요소 관리
+ */
 // 범위 랜덤 함수(소수점 2자리까지)
 function random(min, max) {
-  // `.toFixed()`를 통해 반환된 문자 데이터를,
-  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
   return parseFloat((Math.random() * (max - min) + min).toFixed(2))
 }
-
+// 부유하는(떠 다니는) 요소를 만드는 함수
 function floatingObject(selector, delay, size) {
-  // gsap.to(요소, 시간, 옵션);
   gsap.to(
-    selector, //선택자
-    random(1.5, 2.5),
-    {//옵션
-      y: size,
-      repeat: -1,
-      yoyo: true,
-      ease: Power1.easeInOut,
-      delay: random(0, delay)
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+      ease: Power1.easeInOut // Easing 함수 적용.
     }
-  );
+  )
 }
-floatingObject('.floating1', 1, 15);
-floatingObject('.floating2', .5, 15);
-floatingObject('.floating3', 1.5, 20);
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
 
 /**
  * 요소가 화면에 보여짐 여부에 따른 요소 관리
  */
 // 관리할 요소들 검색!
-const spyEls = document.querySelectorAll('section.scroll-spy'); 
+const spyEls = document.querySelectorAll('section.scroll-spy')
 // 요소들 반복 처리!
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
